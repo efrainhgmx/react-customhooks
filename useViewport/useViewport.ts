810d8d184e54cab8b0b490bc10dc285387cbbc3b
Efrain Hernandez <efrainhg2050@gmail.com>
2022-03-05 15:40:37 -0600

    Get viewport function

--- useViewport/useViewport.ts
+++ useViewport/useViewport.ts
@@ -1,2 +1,15 @@
 //This hook returns the screen size if this is changed
+import { useState, useEffect } from "react";
+
+const getViewport  = () => {
+    const viewPortWidth = document.getElementsByTagName("html")[0].offsetWidth;
+
+    if(viewPortWidth < 768) return "phone";
+
+    if(viewPortWidth < 992) return "tablet";
+
+    if(viewPortWidth < 1200) return "desktop";
+
+    return "largeDesktop";
+};
 
