ccc754f15d393d5f8ae798a567960b0ea05b2d4b
Efrain Hernandez <efrainhg2050@gmail.com>
2022-03-06 17:34:27 -0600

    function for y and x Offset

--- useScroll/useScroll.ts
+++ useScroll/useScroll.ts
@@ -4,4 +4,14 @@ const onScrollHandlers = [];
 
 window.onscroll = function () {
     onScrollHandlers.forEach((h) => h());
-}
+};
+
+export default function useScroll() {
+    const [xOffset, setXOffset] = useState(window.pageXOffset);
+    const [yOffset, setYOffset] = useState(window.pageYOffset);
+    const [xDirection, setXDirection] = useState<"Left" | "Right" | "Initial">(
+      "Initial"
+    );
+    const [yDirection, setYDirection] = useState<"Up" | "Down" | "Initial">(
+      "Initial"
+    );
